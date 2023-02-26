9d7d011960c70f388c853dfe8ba6aae4061ee4b0
Gabriel Fernandes <fernandesbgabriel@gmail.com>
2023-02-26 20:45:41 -0300

    Change API to separate AST, program and machine runtime

--- src/lib.rs
+++ src/lib.rs
@@ -10,51 +10,97 @@ use pest::Parser;
 
 #[derive(Parser)]
 #[grammar = "norma.pest"]
-pub struct NORMAParser;
+struct NORMAParser;
 
-use std::collections::HashMap;
+use std::{collections::HashMap, ops::Deref};
 
 #[derive(Debug)]
-pub enum ASTNorma<'a> {
+pub enum ASTNorma {
     Increment {
-        register: &'a str,
+        register: String,
         next_inst: Option<usize>,
     },
     Decrement {
-        register: &'a str,
+        register: String,
         next_inst: Option<usize>,
     },
     IfZeros {
-        register: &'a str,
+        register: String,
         true_inst: usize,
         false_inst: usize,
     },
 }
 
-type Program<'a> = Vec<ASTNorma<'a>>;
+pub struct NormaProgram {
+    stmts: Vec<ASTNorma>,
+}
 
-impl<'a> ASTNorma<'a> {
-    pub fn parse(source: &'a str) -> Result<Program<'a>, Error<Rule>> {
-        let mut ast = Vec::new();
+pub struct NormaMachine {
+    program: NormaProgram,
+    ctx: Context,
+}
+
+impl NormaProgram {
+    fn new(stmts: Vec<ASTNorma>) -> NormaProgram {
+        NormaProgram { stmts }
+    }
+
+    pub fn parse(source: &str) -> Result<NormaProgram, Box<Error<Rule>>> {
+        let mut stmts = Vec::new();
         let pairs = NORMAParser::parse(Rule::program, source)?;
         for pair in pairs {
-            match pair.as_rule() {
-                Rule::statements => {
-                    ast.push(Self::build_ast_from_statements(pair));
-                }
-                _ => {}
+            if let Rule::statements = pair.as_rule() {
+                stmts.push(ASTNorma::build_ast_from_statements(pair));
             }
         }
 
-        Ok(ast)
+        Ok(Self::new(stmts))
+    }
+}
+
+impl Deref for NormaProgram {
+    type Target = Vec<ASTNorma>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.stmts
+    }
+}
+
+impl NormaMachine {
+    pub fn new(prg: NormaProgram) -> NormaMachine {
+        Self::from_context(prg, Context::default())
     }
 
-    fn build_ast_from_statements(pair: pest::iterators::Pair<'a, Rule>) -> Self {
+    pub fn from_context(program: NormaProgram, ctx: Context) -> NormaMachine {
+        NormaMachine { program, ctx }
+    }
+
+    pub fn run(&mut self) {
+        while self.run_bound().is_some() {}
+    }
+
+    pub fn run_bound(&mut self) -> Option<()> {
+        let instruct = self.ctx.get_cursor();
+        let stmt = self.program.get(instruct);
+        let r = match stmt {
+            Some(stmt) => {
+                stmt.execute(&mut self.ctx);
+                Some(())
+            }
+            None => None,
+        };
+        self.ctx.inst += 1;
+        r
+    }
+}
+
+impl ASTNorma {
+    fn build_ast_from_statements(pair: pest::iterators::Pair<Rule>) -> Self {
         let pair = pair.into_inner().next().unwrap();
         match pair.as_rule() {
             Rule::inc => {
                 let mut pair = pair.into_inner();
-                let register = pair.next().unwrap().as_str();
+                let register = pair.next().unwrap().as_str().into();
                 let next_inst = pair.next().map(|inst| inst.as_str().parse().unwrap());
                 Self::Increment {
                     register,
@@ -63,7 +109,7 @@ impl<'a> ASTNorma<'a> {
             }
             Rule::dec => {
                 let mut pair = pair.into_inner();
-                let register = pair.next().unwrap().as_str();
+                let register = pair.next().unwrap().as_str().into();
                 let next_inst = pair.next().map(|inst| inst.as_str().parse().unwrap());
                 Self::Decrement {
                     register,
@@ -72,26 +118,26 @@ impl<'a> ASTNorma<'a> {
             }
             Rule::ifz => {
                 let mut pair = pair.into_inner();
-                let register = pair.next().unwrap().as_str();
-                let true_inst = pair
+                let register = pair.next().unwrap().as_str().into();
+                let true_inst: usize = pair
                     .next()
                     .map(|inst| inst.as_str().parse().unwrap())
                     .unwrap();
-                let false_inst = pair
+                let false_inst: usize = pair
                     .next()
                     .map(|inst| inst.as_str().parse().unwrap())
                     .unwrap();
                 Self::IfZeros {
                     register,
-                    true_inst,
-                    false_inst,
+                    true_inst: true_inst - 1,
+                    false_inst: false_inst - 1,
                 }
             }
-            unknown_expr => panic!("Unexpected statements: {:?}", unknown_expr),
+            unknown_expr => panic!("Unexpected statements: {unknown_expr:?}"),
         }
     }
 
-    fn run(&self, context: &mut Context) {
+    fn execute(&self, context: &mut Context) {
         match self {
             ASTNorma::Increment {
                 register,
@@ -118,11 +164,12 @@ impl<'a> ASTNorma<'a> {
                 true_inst,
                 false_inst,
             } => {
-                context.cursor = if *context.get_register(register) == BigUint::zero() {
+                let next = if *context.get_register(register) == BigUint::zero() {
                     *true_inst
                 } else {
                     *false_inst
-                }
+                };
+                context.cursor = next;
             }
         }
     }
@@ -132,14 +179,16 @@ pub struct NORMAInterpreter;
 
 #[derive(Debug)]
 pub struct Context {
-    pub cursor: usize,
+    inst: usize,
+    cursor: usize,
     registers: HashMap<String, BigUint>,
 }
 
 impl Context {
     pub fn new() -> Self {
         Self {
-            cursor: 1,
+            inst: 0,
+            cursor: 0,
             registers: HashMap::new(),
         }
     }
@@ -156,6 +205,11 @@ impl Context {
         }
     }
 
+    #[cfg(test)]
+    fn get_register_read_only(&self, register: &str) -> BigUint {
+        self.registers.get(register).cloned().unwrap_or_default()
+    }
+
     fn get_register(&mut self, register: &str) -> &mut BigUint {
         use std::collections::hash_map::Entry;
         match self.registers.entry(register.into()) {
@@ -164,36 +218,59 @@ impl Context {
         }
     }
 
+    pub fn get_inst(&self) -> usize {
+        self.inst
+    }
+
+    pub fn get_cursor(&self) -> usize {
+        self.cursor
+    }
+
     pub fn get_registers(&self) -> Vec<(String, BigUint)> {
         self.registers.clone().into_iter().collect()
     }
+}
 
-    pub fn run(&mut self, prg: &Program) {
-        while self.run_bound(prg) != None {}
-    }
-
-    pub fn run_bound(&mut self, prg: &Program) -> Option<()> {
-        let instruct = self.cursor;
-        let stmt = prg.get(instruct);
-        match stmt {
-            Some(stmt) => {
-                stmt.run(self);
-                Some(())
-            }
-            None => None,
-        }
+impl Default for Context {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
-impl<'a> NORMAInterpreter {
-    pub fn run(prg: &'a Program) -> Context {
-        let mut context = Context::new();
-        context.run(prg);
-        context
-    }
-}
+#[cfg(test)]
+mod tests {
+    use num_traits::ToPrimitive;
 
-#[test]
-fn test_basic() {
-    // ...
+    use crate::{NormaMachine, NormaProgram};
+
+    #[test]
+    fn test_basic_inc() {
+        let vm = parse_and_run("inc A\ninc A");
+        assert_eq!(2, vm.ctx.get_register_read_only("A").to_isize().unwrap());
+    }
+
+    #[test]
+    fn test_basic_dec() {
+        let vm = parse_and_run("inc A\ninc A\ndec A");
+        assert_eq!(1, vm.ctx.get_register_read_only("A").to_isize().unwrap());
+    }
+
+    #[test]
+    fn test_basic_ifz() {
+        let vm = parse_and_run("inc A\nifz A (3,4)\ninc B\ninc C");
+        assert_eq!(1, vm.ctx.get_register_read_only("A").to_isize().unwrap());
+        assert_eq!(0, vm.ctx.get_register_read_only("B").to_isize().unwrap());
+        assert_eq!(1, vm.ctx.get_register_read_only("C").to_isize().unwrap());
+        let vm = parse_and_run("inc C\nifz A (3,4)\ninc B\ninc C");
+        assert_eq!(0, vm.ctx.get_register_read_only("A").to_isize().unwrap());
+        assert_eq!(1, vm.ctx.get_register_read_only("B").to_isize().unwrap());
+        assert_eq!(2, vm.ctx.get_register_read_only("C").to_isize().unwrap());
+    }
+
+    fn parse_and_run(source: &str) -> NormaMachine {
+        let prg = NormaProgram::parse(source).unwrap();
+        let mut vm = NormaMachine::new(prg);
+        vm.run();
+        vm
+    }
 }
